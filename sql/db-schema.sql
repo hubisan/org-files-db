e9f0d9f840c1f698fb94b0fe8f972a25964e09d9
hubisan <hubisan@gmail.com>
2024-10-24 23:55:41 +0200

    wip

--- sql/db-schema.sql
+++ sql/db-schema.sql
@@ -14,53 +14,36 @@ CREATE TABLE IF NOT EXISTS files (
 );
 
 -- Table to store headings extracted from Org files
--- A level 0 heading is used at file-level.
 CREATE TABLE IF NOT EXISTS headings (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  -- Foreign key referencing the 'files' table.
   file_id INTEGER NOT NULL,
-  -- Heading level (0 for file-level, 1 for top-level, 2 for subheading, etc.).
   level INTEGER NOT NULL,
-  -- Point of where the headings begins in the file.
   begin INTEGER NOT NULL,
-  --Title without statistic cookies and emphasis markers, and links converted to text.
   title TEXT,
-  -- Raw title of the heading, excludes only TODO keyword.
   title_raw TEXT,
-  -- Other components.
   priority TEXT,
   todo_keyword TEXT,
   todo_type TEXT,
   archivedp INTEGER,
   footnote_section_p INTEGER
-  -- Parent heading ID, referencing another entry in this table.
   parent_id INTEGER,
-  UNIQUE (file_id, point),
   FOREIGN KEY (file_id) REFERENCES files (id) ON DELETE CASCADE,
   FOREIGN KEY (parent_id) REFERENCES headings (id) ON DELETE CASCADE
 );
 
-
 --  Table to store tags for headings.
 CREATE TABLE IF NOT EXISTS tags (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  -- Foreign key referencing the 'headings' table.
   heading_id INTEGER NOT NULL,
-  -- Tag name.
   tag TEXT NOT NULL,
-  -- Set up cascading delete for referenced heading.
   FOREIGN KEY (heading_id) REFERENCES headings (id) ON DELETE CASCADE
 );
 
 -- Table to store file-level keywords
 CREATE TABLE IF NOT EXISTS keywords (
   id integer PRIMARY KEY AUTOINCREMENT,
-  -- Foreign key referencing the 'headings' table.
-  -- Keywords are stored in a heading with level 0.
   heading_id INTEGER NOT NULL,
-  -- Keyword name.
   keyword TEXT NOT NULL,
-  -- Associated value for the keyword.
   value TEXT,
   FOREIGN KEY (heading_id) REFERENCES headings (id) ON DELETE CASCADE
 );
@@ -70,7 +53,7 @@ CREATE TABLE IF NOT EXISTS properties (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   heading_id INTEGER NOT NULL,
   key TEXT NOT NULL,
-  value TEXT NOT NULL,
+  value TEXT,
   FOREIGN KEY (heading_id) REFERENCES headings(id) ON DELETE CASCADE
 );
 
@@ -86,8 +69,41 @@ CREATE TABLE IF NOT EXISTS links (
   description TEXT,
   format TEXT,
   search_option TEXT,
-  UNIQUE (heading_id, begin),
   FOREIGN KEY (heading_id) REFERENCES headings(id) ON DELETE CASCADE
+);
+
+-- Table to store general timestamp information for headings
+CREATE TABLE IF NOT EXISTS timestamps (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    heading_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    range_type TEXT,
+    raw_value TEXT NOT NULL,
+    FOREIGN KEY (heading_id) REFERENCES headings(id) ON DELETE CASCADE
+);
+
+-- Table to store timestamp information
+CREATE TABLE IF NOT EXISTS timestamps (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    heading_id INTEGER NOT NULL,
+    start_timestamp REAL NOT NULL,
+    end_timestamp REAL,
+    type TEXT NOT NULL,
+    range_type TEXT,
+    raw_value TEXT NOT NULL,
+    FOREIGN KEY (heading_id) REFERENCES headings(id) ON DELETE CASCADE
+);
+
+-- Table to store repeater information for timestamps
+CREATE TABLE IF NOT EXISTS repeater_timestamps (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    value INTEGER,
+    unit TEXT,
+    deadline_value INTEGER,
+    deadline_unit TEXT,
+    FOREIGN KEY (timestamps) REFERENCES timestamps(id) ON DELETE CASCADE
 );
 
 COMMIT;
