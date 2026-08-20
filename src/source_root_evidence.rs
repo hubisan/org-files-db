160947aed097df2a8f25cf38242d7be52840a543
hubisan <hubisan@gmail.com>
2026-08-21 01:00:16 +0200

    fix: error with new clippy version

--- src/source_root_evidence.rs
+++ src/source_root_evidence.rs
@@ -374,7 +374,9 @@ fn decode_hex(value: &str) -> Option<Vec<u8>> {
     }
     value
         .as_bytes()
-        .chunks_exact(2)
+        .as_chunks::<2>()
+        .0
+        .iter()
         .map(|pair| Some((hex_value(pair[0])? << 4) | hex_value(pair[1])?))
         .collect()
 }
